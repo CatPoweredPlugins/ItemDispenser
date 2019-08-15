57135d6685a05657bcfa9cd230b2479a50eea581
Ryzhehvost <kotlyar.andrey@gmail.com>
2019-08-15 09:03:49 +0300

    empty project that compiles

--- ItemDispenser/ItemDispenser.cs
+++ ItemDispenser/ItemDispenser.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Composition;
+using System.Threading.Tasks;
+using ArchiSteamFarm;
+using ArchiSteamFarm.Json;
+using ArchiSteamFarm.Plugins;
+using JetBrains.Annotations;
 
 namespace ItemDispenser
 {
-    public class Class1
-    {
-    }
+	[Export(typeof(IPlugin))]
+	public class ItemDispenser : IBotTradeOffer {
+		public string Name => throw new NotImplementedException();
+
+		public Version Version => throw new NotImplementedException();
+
+		public Task<bool> OnBotTradeOffer([NotNull] Bot bot, [NotNull] Steam.TradeOffer tradeOffer) => throw new NotImplementedException();
+		public void OnLoaded() => throw new NotImplementedException();
+	}
 }
--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 del .\ItemDispenser\*.zip
-dotnet publish -c "Release" -f "net472" -o "out/generic-netf"
+dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
 rename .\ItemDispenser\ItemDispenser.zip ItemDispenser-netf.zip 
 dotnet publish -c "Release" -f "netcoreapp2.2" -o "out/generic" "/p:LinkDuringPublish=false"
