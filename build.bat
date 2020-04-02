93131dbbcfe25e32907b1b07a3e66348f1023f66
Ryzhehvost <kotlyar.andrey@gmail.com>
2020-04-02 22:17:12 +0300

    update build script

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+if not exist ArchiSteamFarm\ArchiSteamFarm (git submodule update --init)
+git submodule foreach "git fetch origin; git checkout $(git rev-list --tags --max-count=1);"
 del .\ItemDispenser\*.zip
 dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
 rename .\ItemDispenser\ItemDispenser.zip ItemDispenser-netf.zip 
-dotnet publish -c "Release" -f "netcoreapp3.0" -o "out/generic" "/p:LinkDuringPublish=false"
+dotnet publish -c "Release" -f "netcoreapp3.1" -o "out/generic" "/p:LinkDuringPublish=false"
