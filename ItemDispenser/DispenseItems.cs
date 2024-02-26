435d4910248fbc18fe5ac7ee891c5be3c40486ee
Archi <JustArchi@JustArchi.net>
2024-02-26 17:25:49 +0100

    Fix Rudo not following any code style

--- ItemDispenser/DispenseItems.cs
+++ ItemDispenser/DispenseItems.cs
@@ -10,14 +10,14 @@ internal sealed class DispenseItem {
 	[JsonInclude]
 	[JsonRequired]
 	internal uint AppID { get; private init; }
-		
+
 	[JsonInclude]
 	[JsonRequired]
 	internal ulong ContextID { get; private init; }
 
 	[JsonInclude]
 	internal ImmutableHashSet<Asset.EType> Types { get; private init; } = ImmutableHashSet<Asset.EType>.Empty;
-	
+
 	[JsonConstructor]
 	private DispenseItem() { }
 }
--- ItemDispenser/ItemDispenser.cs
+++ ItemDispenser/ItemDispenser.cs
@@ -19,103 +19,88 @@ namespace ItemDispenser;
 
 [Export(typeof(IPlugin))]
 [SuppressMessage("ReSharper", "UnusedType.Global")]
-internal sealed class ItemDispenser : IBotTradeOffer, IBotModules
-{
-    private static readonly ConcurrentDictionary<Bot, FrozenDictionary<(uint AppID, ulong ContextID), FrozenSet<Asset.EType>>> BotSettings = new();
+internal sealed class ItemDispenser : IBotTradeOffer, IBotModules {
+	private static readonly ConcurrentDictionary<Bot, FrozenDictionary<(uint AppID, ulong ContextID), FrozenSet<Asset.EType>>> BotSettings = new();
 
-    public Task OnBotInitModules(Bot bot, IReadOnlyDictionary<string, JsonElement>? additionalConfigProperties)
-    {
-        ArgumentNullException.ThrowIfNull(bot);
-        
-        if (additionalConfigProperties == null)
-        {
-            BotSettings.TryRemove(bot, out _);
-            
-            return Task.CompletedTask;
-        }
+	public string Name => nameof(ItemDispenser);
 
-        if (!additionalConfigProperties.TryGetValue("Rudokhvist.DispenseItems", out var jsonElement))
-        {
-            BotSettings.TryRemove(bot, out _);
-            
-            return Task.CompletedTask;
-        }
+	public Version Version => typeof(ItemDispenser).Assembly.GetName().Version ?? new Version("0.0.0.0");
 
-        Dictionary<(uint AppID, ulong ContextID), HashSet<Asset.EType>> botSettings = new();
-        
-        try
-        {
-            foreach (var dispenseItem in jsonElement.EnumerateArray().Select(static elem => elem.ToJsonObject<DispenseItem>()))
-            {
-                if (dispenseItem == null)
-                {
-                    continue;
-                }
+	public Task OnBotInitModules(Bot bot, IReadOnlyDictionary<string, JsonElement>? additionalConfigProperties) {
+		ArgumentNullException.ThrowIfNull(bot);
 
-                var key = (dispenseItem.AppID, dispenseItem.ContextID);
+		if (additionalConfigProperties == null) {
+			BotSettings.TryRemove(bot, out _);
 
-                if (!botSettings.TryGetValue(key, out var types))
-                {
-                    types = new HashSet<Asset.EType>();
-                    botSettings[key] = types;
-                }
-                
-                types.UnionWith(dispenseItem.Types);
-            }
+			return Task.CompletedTask;
+		}
 
-            BotSettings[bot] = botSettings.ToFrozenDictionary(kv => kv.Key, kv => kv.Value.ToFrozenSet());
-        }
-        catch (Exception e)
-        {
-            bot.ArchiLogger.LogGenericException(e);
-            bot.ArchiLogger.LogGenericError("Item Dispenser configuration is wrong!");
-            
-            BotSettings.TryRemove(bot, out _);
-        }
+		if (!additionalConfigProperties.TryGetValue("Rudokhvist.DispenseItems", out JsonElement jsonElement)) {
+			BotSettings.TryRemove(bot, out _);
 
-        return Task.CompletedTask;
-    }
+			return Task.CompletedTask;
+		}
 
-    public string Name => nameof(ItemDispenser);
+		Dictionary<(uint AppID, ulong ContextID), HashSet<Asset.EType>> botSettings = new();
 
-    public Version Version => typeof(ItemDispenser).Assembly.GetName().Version ?? new Version("0.0.0.0");
+		try {
+			foreach (DispenseItem? dispenseItem in jsonElement.EnumerateArray().Select(static elem => elem.ToJsonObject<DispenseItem>())) {
+				if (dispenseItem == null) {
+					continue;
+				}
 
-    public async Task<bool> OnBotTradeOffer(Bot bot, TradeOffer tradeOffer)
-    {
-        ArgumentNullException.ThrowIfNull(bot);
-        ArgumentNullException.ThrowIfNull(tradeOffer);
+				(uint AppID, ulong ContextID) key = (dispenseItem.AppID, dispenseItem.ContextID);
 
-        if (!BotSettings.TryGetValue(bot, out var itemsToDispense))
-        {
-            // Settings not declared for this bot, skip overhead
-            return false;
-        }
+				if (!botSettings.TryGetValue(key, out HashSet<Asset.EType>? types)) {
+					types = new HashSet<Asset.EType>();
+					botSettings[key] = types;
+				}
 
-        // If we're receiving something in return, and donations is not accepted - ignore
-        if (tradeOffer.ItemsToReceiveReadOnly.Count > 0 && !bot.BotConfig.TradingPreferences.HasFlag(BotConfig.ETradingPreferences.AcceptDonations))
-        {
-            return false;
-        }
+				types.UnionWith(dispenseItem.Types);
+			}
 
-        var holdDuration = await bot.GetTradeHoldDuration(tradeOffer.OtherSteamID64, tradeOffer.TradeOfferID).ConfigureAwait(false);
+			BotSettings[bot] = botSettings.ToFrozenDictionary(static kv => kv.Key, static kv => kv.Value.ToFrozenSet());
+		} catch (Exception e) {
+			bot.ArchiLogger.LogGenericException(e);
+			bot.ArchiLogger.LogGenericError("Item Dispenser configuration is wrong!");
 
-        // If user has a trade hold, we add extra logic
-        // If trade hold duration exceeds our max, or user asks for cards with short lifespan, reject the trade
-        // If we can't get trade hold duration, ignore
-        switch (holdDuration)
-        {
-            case null:
-            case > 0 when holdDuration.Value > (ASF.GlobalConfig?.MaxTradeHoldDuration ?? 0) || tradeOffer.ItemsToGiveReadOnly.Any(item => item.Type is Asset.EType.FoilTradingCard or Asset.EType.TradingCard && CardsFarmer.SalesBlacklist.Contains(item.RealAppID)):
-                return false;
-        }
+			BotSettings.TryRemove(bot, out _);
+		}
 
-        return tradeOffer.ItemsToGiveReadOnly.All(item => itemsToDispense.TryGetValue((item.AppID, item.ContextID), out var dispense) && (dispense.Count == 0 || dispense.Contains(item.Type)));
-    }
+		return Task.CompletedTask;
+	}
 
-    public Task OnLoaded()
-    {
-        ASF.ArchiLogger.LogGenericInfo("Item Dispenser Plugin by Rudokhvist, powered by ginger cats");
-        
-        return Task.CompletedTask;
-    }
+	public async Task<bool> OnBotTradeOffer(Bot bot, TradeOffer tradeOffer) {
+		ArgumentNullException.ThrowIfNull(bot);
+		ArgumentNullException.ThrowIfNull(tradeOffer);
+
+		if (!BotSettings.TryGetValue(bot, out FrozenDictionary<(uint AppID, ulong ContextID), FrozenSet<Asset.EType>>? itemsToDispense)) {
+			// Settings not declared for this bot, skip overhead
+			return false;
+		}
+
+		// If we're receiving something in return, and donations is not accepted - ignore
+		if ((tradeOffer.ItemsToReceiveReadOnly.Count > 0) && !bot.BotConfig.TradingPreferences.HasFlag(BotConfig.ETradingPreferences.AcceptDonations)) {
+			return false;
+		}
+
+		byte? holdDuration = await bot.GetTradeHoldDuration(tradeOffer.OtherSteamID64, tradeOffer.TradeOfferID).ConfigureAwait(false);
+
+		// If user has a trade hold, we add extra logic
+		// If trade hold duration exceeds our max, or user asks for cards with short lifespan, reject the trade
+		// If we can't get trade hold duration, ignore
+		switch (holdDuration) {
+			case null:
+			case > 0 when (holdDuration.Value > (ASF.GlobalConfig?.MaxTradeHoldDuration ?? 0)) || tradeOffer.ItemsToGiveReadOnly.Any(static item => item.Type is Asset.EType.FoilTradingCard or Asset.EType.TradingCard && CardsFarmer.SalesBlacklist.Contains(item.RealAppID)):
+				return false;
+		}
+
+		return tradeOffer.ItemsToGiveReadOnly.All(item => itemsToDispense.TryGetValue((item.AppID, item.ContextID), out FrozenSet<Asset.EType>? dispense) && ((dispense.Count == 0) || dispense.Contains(item.Type)));
+	}
+
+	public Task OnLoaded() {
+		ASF.ArchiLogger.LogGenericInfo("Item Dispenser Plugin by Rudokhvist, powered by ginger cats");
+
+		return Task.CompletedTask;
+	}
 }
